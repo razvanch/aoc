69ef3c7f8dfdd8516885b0f018a05a206a867b6c
Razvan Chitu <razvan.ch95@gmail.com>
2017-12-10 15:56:36 +0200

    Add 03

---
+++ 03_01.py
@@ -0,0 +1,9 @@
+# http://adventofcode.com/2017/day/3
+
+number = 347991
+
+distance = 0
+
+# your code goes in here
+
+print(distance)
---
+++ 03_02.py
@@ -0,0 +1,9 @@
+# http://adventofcode.com/2017/day/3
+
+number = 347991
+
+first_largest = 0
+
+# your code goes in here
+
+print(first_largest)
