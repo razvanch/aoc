a6bd74f76478e0320ed346a4653295115e54922e
Razvan Chitu <razvan.ch95@gmail.com>
2017-12-10 15:12:36 +0200

    Add 01 stubs

---
+++ 01_01.py
@@ -0,0 +1,10 @@
+# http://adventofcode.com/2017/day/1
+
+with open("01_01.in") as f:
+    digits = [int(digit) for digit in next(f).strip()]
+
+captcha = 0
+
+# your code goes in here
+
+print(captcha)
