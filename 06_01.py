9348f256f8ec36413c3fd9214709f748941433eb
Razvan Chitu <razvan.ch95@gmail.com>
2017-12-10 16:46:25 +0200

    Add 06

---
+++ 06_01.py
@@ -0,0 +1,10 @@
+# http://adventofcode.com/2017/day/6
+
+with open("06_01.in") as f:
+    blocks = [int(token) for token in next(f).strip().split() if token]
+
+steps = 0
+
+# your code goes in here
+
+print(steps)
